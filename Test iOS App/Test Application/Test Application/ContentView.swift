8314b0316bf6ac615371c4c2ed03468e0054b292
infosecmember <52580853+infosecmember@users.noreply.github.com>
2019-10-10 12:29:30 -0700

    Initial Add

---
+++ Test iOS App/Test Application/Test Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Application
+//
+//  Created by otopgul on 10/10/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
